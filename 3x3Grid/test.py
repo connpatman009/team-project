eeb0f0fc545726b3c1ca31aabdcd159395ed178f
DBD18 <70073181+DBD18@users.noreply.github.com>
2022-11-14 13:45:36 -0500

    Initial TraCI Commit

---
+++ 3x3Grid/test.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import optparse
+
+# VERIFY THAT SUMO IS INSTALLED AND PATH IS SET UP
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+else:
+    sys.exit("ERROR: Please declare SUMO_HOME in your environment variables.")
+
+from sumolib import checkBinary
+import traci
+
+def get_options():
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--nogui", action="store_true", default=False, help="run the commandline version of SUMO")
+    options, args = opt_parser.parse_args()
+    return options
+
+# TraCI Control Loop
+def run():
+
+    step = 0
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+
+        # ---------- DO TRACI THINGS HERE ----------
+        
+        # Example -> print(step)
+
+
+        # ----------------------------------
+
+        step += 1
+
+    traci.close()
+    sys.stdout.flush()
+    pass
+
+# Main
+if __name__ == "__main__":
+    options = get_options()
+
+    # check binary
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else: sumoBinary = checkBinary('sumo-gui')
+
+    # TraCI starts sumo as a subprocess and then this script connects and runs
+    traci.start([sumoBinary, "-c", "3dfasdfasdfg", "--tripinfo-output", "tripinfo.xml"])
+    run()
---
+++ TraCI/traci_template.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import optparse
+
+# VERIFY THAT SUMO IS INSTALLED AND PATH IS SET UP
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+else:
+    sys.exit("ERROR: Please declare SUMO_HOME in your environment variables.")
+
+from sumolib import checkBinary
+import traci
+
+def get_options():
+    opt_parser = optparse.OptionParser()
+    opt_parser.add_option("--nogui", action="store_true", default=False, help="run the commandline version of SUMO")
+    options, args = opt_parser.parse_args()
+    return options
+
+# TraCI Control Loop
+def run():
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        
+    traci.close()
+    sys.stdout.flush()
+    pass
+
+# Main
+if __name__ == "__main__":
+    options = get_options()
+
+    # check binary
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else: sumoBinary = checkBinary('sumo-gui')
+
+    # TraCI starts sumo as a subprocess and then this script connects and runs
+    traci.start([sumoBinary, "-c", "<<SUMOCFG FILENAME HERE>>", "--tripinfo-output", "tripinfo.xml"])
+    run()
