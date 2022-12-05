b8125ab337e859619157eb0be6031a0c74b5898f
DBD18 <70073181+DBD18@users.noreply.github.com>
2022-12-05 18:34:45 -0500

    Updated Logic of Runner

--- VerticalRoad/EMS_POLICY_SCENARIOS.py
+++ VerticalRoad/EMS_POLICY_SCENARIOS.py
@@ -81,12 +81,20 @@ def run(sumo_gui, FILENAME, policy_type):
     traci.start([sumo_gui, "-c", FILENAME])
     step = 0
     intersection_ids = traci.trafficlight.getIDList()
-    hit_1 = False
-    hit_2 = False
-    hit_3 = False
-    hit_4 = False
 
-    already_hit = False
+    det1_FAR    = False
+    det1_NEAR   = False
+    det2_FAR    = False
+    det2_NEAR   = False
+    det3_FAR    = False
+    det3_NEAR   = False
+    det4_FAR    = False
+    det4_NEAR   = False
+
+    finished = False
+
+    J3_normaled = False
+    J4_normaled = False
 
     count_EMS_time_steps = 0
 
@@ -95,89 +103,127 @@ def run(sumo_gui, FILENAME, policy_type):
         if(isEMSPresent(traci.vehicle.getIDList())):
             count_EMS_time_steps += 1
         # DO TRACI THINGS HERE
-        in_det_1 = traci.multientryexit.getLastStepVehicleNumber('det1')
-        in_det_2 = traci.multientryexit.getLastStepVehicleNumber('det2')
-        in_det_3 = traci.multientryexit.getLastStepVehicleNumber('det3')
-        in_det_4 = traci.multientryexit.getLastStepVehicleNumber('det4')
-        if in_det_1:
-            hit_1 = True
-        if in_det_2:
-            hit_2 = True
-        if in_det_3:
-            hit_3 = True
-        if in_det_4:
-            hit_4 = True
-        
-        # At this point, hit_X will be true if EMS has hit detector X, so
-        if hit_1 and not hit_2:
-            # Set intersection 1 to ems policy
-            ems_policy(policy_type, intersection_ids[0])
+        if not det1_FAR:
+            det1_FAR    = traci.multientryexit.getLastStepVehicleNumber('det1_FAR')
+        if not det1_NEAR:
+            det1_NEAR   = traci.multientryexit.getLastStepVehicleNumber('det1_NEAR')
+        if not det2_FAR:
+            det2_FAR    = traci.multientryexit.getLastStepVehicleNumber('det2_FAR')
+        if not det2_NEAR:
+            det2_NEAR   = traci.multientryexit.getLastStepVehicleNumber('det2_NEAR')
+        if not det3_FAR:
+            det3_FAR    = traci.multientryexit.getLastStepVehicleNumber('det3_FAR')
+        if not det3_NEAR:
+            det3_NEAR   = traci.multientryexit.getLastStepVehicleNumber('det3_NEAR')
+        if not det4_FAR:
+            det4_FAR    = traci.multientryexit.getLastStepVehicleNumber('det4_FAR')
+        if not det4_NEAR:
+            det4_NEAR   = traci.multientryexit.getLastStepVehicleNumber('det4_NEAR')
+
+        if policy_type == 'gc':
+
+            if not finished:
+                if det1_FAR and not det2_FAR:
+                    ems_policy(policy_type, 'J3')
+                elif det2_FAR and not det3_FAR:
+                    ems_policy(policy_type, 'J4')
+                    if not J3_normaled:
+                        return_to_normal('J3')
+                        J3_normaled = True
+                elif det3_FAR and not det4_FAR:
+                    ems_policy(policy_type, 'J5')
+                    if not J4_normaled:
+                        return_to_normal('J4')
+                        J4_normaled = True
+                elif det4_FAR:
+                    return_to_normal('J5')
+                    finished = True
+
+        elif policy_type == 'rf':
+
+            if not finished:
+                if det1_NEAR and not det2_FAR:
+                    ems_policy(policy_type, 'J3')
+                elif det2_FAR and not det2_NEAR:
+                    if not J3_normaled:
+                        return_to_normal('J3')
+                        J3_normaled = True
+                elif det2_NEAR and not det3_FAR:
+                    ems_policy(policy_type, 'J4')
+                elif det3_FAR and not det3_NEAR:
+                    if not J4_normaled:
+                        return_to_normal('J4')
+                        J4_normaled = True
+                elif det3_NEAR and not det4_FAR:
+                    ems_policy(policy_type, 'J5')
+                elif det4_FAR:
+                    return_to_normal('J5')
+                    finished = True
+
+        elif policy_type == 'na':
             pass
-        elif hit_2 and not hit_3:
-            # Return intersection 1 to normal
-            # Set intersection 2 to ems policy
-            return_to_normal(intersection_ids[0])
-            ems_policy(policy_type, intersection_ids[1])
-            pass
-        elif hit_3 and not hit_4:
-            # Return intersection 2 to normal
-            # Set intersection 3 to ems policy
-            return_to_normal(intersection_ids[1])
-            ems_policy(policy_type, intersection_ids[2])
-            pass
-        elif hit_4 and not already_hit:
-            # Return intersection 3 to normal
-            return_to_normal(intersection_ids[2])
-            already_hit = True
-            pass
-        step += 1
+        else:
+            raise Exception("{} is not a valid policy...".format(policy_type))
+        step+=1
     traci.close()
     sys.stdout.flush()
     return count_EMS_time_steps, step
 
-def run_all_policies_experiment(sumo_gui, FILENAME):
+def run_all_policies_experiment(sumo_gui, FILENAMES):
+
     print('Running GREEN CORRIDOR policy...')
-    gc_ems_steps, gc_total_steps = run(sumo_gui, FILENAME, 'gc')
+    light_gc_ems_steps, light_gc_total_steps = run(sumo_gui, FILENAMES[0], 'gc')
     print('Running RED FREEZE policy...')
-    rf_ems_steps, rf_total_steps = run(sumo_gui, FILENAME, 'rf')
+    light_rf_ems_steps, light_rf_total_steps = run(sumo_gui, FILENAMES[0], 'rf')
     print('Running NORMAL policy (no traffic light manipulation)...')
-    na_ems_steps, na_total_steps = run(sumo_gui, FILENAME, 'na')
+    light_na_ems_steps, light_na_total_steps = run(sumo_gui, FILENAMES[0], 'na')
+
+    print('Running GREEN CORRIDOR policy...')
+    average_gc_ems_steps, average_gc_total_steps = run(sumo_gui, FILENAMES[1], 'gc')
+    print('Running RED FREEZE policy...')
+    average_rf_ems_steps, average_rf_total_steps = run(sumo_gui, FILENAMES[1], 'rf')
+    print('Running NORMAL policy (no traffic average manipulation)...')
+    average_na_ems_steps, average_na_total_steps = run(sumo_gui, FILENAMES[1], 'na')
+
+    print('Running GREEN CORRIDOR policy...')
+    heavy_gc_ems_steps, heavy_gc_total_steps = run(sumo_gui, FILENAMES[2], 'gc')
+    print('Running RED FREEZE policy...')
+    heavy_rf_ems_steps, heavy_rf_total_steps = run(sumo_gui, FILENAMES[2], 'rf')
+    print('Running NORMAL policy (no traffic light manipulation)...')
+    heavy_na_ems_steps, heavy_na_total_steps = run(sumo_gui, FILENAMES[2], 'na')
 
     print('\n\n---------------- RESULTS ----------------\n')
-    print('GREEN CORRIDOR:\nEMS travel time = {} steps\nCongestion clearing time = {} steps\n'.format(gc_ems_steps, gc_total_steps))
-    print('RED FREEZE:\nEMS travel time = {} steps\nCongestion clearing time = {} steps\n'.format(rf_ems_steps, rf_total_steps))
-    print('CONTROL (no traffic light manipulation):\nEMS travel time = {} steps\nCongestion clearing time = {} steps\n'.format(na_ems_steps, na_total_steps))
+    print('{}:\n'.format(FILENAMES[0]))
+    print('\tGREEN CORRIDOR:\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(light_gc_ems_steps, light_gc_total_steps))
+    print('\tRED FREEZE:\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(light_rf_ems_steps, light_rf_total_steps))
+    print('\tCONTROL (no traffic light manipulation):\nEMS travel time = {} steps\nCongestion clearing time = {} steps\n'.format(light_na_ems_steps, light_na_total_steps))
+
+    print('{}:\n'.format(FILENAMES[1]))
+    print('\tGREEN CORRIDOR:\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(average_gc_ems_steps, average_gc_total_steps))
+    print('\tRED FREEZE:\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(average_rf_ems_steps, average_rf_total_steps))
+    print('\tCONTROL (no traffic light manipulation):\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(average_na_ems_steps, average_na_total_steps))
+
+    print('{}:\n'.format(FILENAMES[2]))
+    print('\tGREEN CORRIDOR:\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(heavy_gc_ems_steps, heavy_gc_total_steps))
+    print('\tRED FREEZE:\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(heavy_rf_ems_steps, heavy_rf_total_steps))
+    print('\tCONTROL (no traffic light manipulation):\n\t\tEMS travel time = {} steps\n\t\tCongestion clearing time = {} steps\n'.format(heavy_na_ems_steps, heavy_na_total_steps))
     print('-----------------------------------------\n')
 # Main
 if __name__ == "__main__":
 
     options = get_options()
-    FILENAME = "simulation.sumocfg"
+    FILENAMES = ["simulation_LIGHT.sumocfg", "simulation_AVERAGE.sumocfg", "simulation_HEAVY.sumocfg"]
 
     # check binary
     if options.nogui:
         sumoBinary = checkBinary('sumo')
     else: sumoBinary = checkBinary('sumo-gui')
 
-    cmd =   [   sumoBinary,
-                "-c",
-                FILENAME,
-                "--tripinfo-output",
-                "tripinfo.xml"
-            ]
+    user_input = input("\nRun with gui? [Y/N]\n\n")
 
-    user_input = input("\nRun auto-experiment? [Y/N]\n\n")
-        
     if user_input.lower() == 'y':
-        sumo_gui = 'sumo'
-        run_all_policies_experiment(sumo_gui, FILENAME)
-        pass
+        sumo_gui = 'sumo-gui'
     else:
-        sumo_gui = input("\nRun sumo-gui? [Y/N]\n\n")
-        if sumo_gui.lower() == 'y':
-            sumo_gui = 'sumo-gui'
-        else:
-            sumo_gui = 'sumo'
-        policty_type = input("\nPlease type your preffered policy: <gc (green corridor) | rf (red freeze) | na (normal lights)>\n\n")
-        EMSTravelTime, TotalCongestionClearingTime = run(sumo_gui, FILENAME, policty_type)
-        print('\nEMS travel time = {} steps\nCongestion clearing time = {} steps\n'.format(EMSTravelTime, TotalCongestionClearingTime))
+        sumo_gui = 'sumo'
+
+    run_all_policies_experiment(sumo_gui, FILENAMES)
